158c80fccbaef5023a3b30e261628139e084ee1a
ankitrajput9981 <54628091+ankitrajput9981@users.noreply.github.com>
2022-03-27 18:47:33 +0530

    Update conn.js

--- src/conn.js
+++ src/conn.js
@@ -1,5 +1,5 @@
 const mongoose =  require("mongoose");
-mongoose.connect("mongodb://localhost:27017/portfolio",{
+mongoose.connect("mongodb+srv://Ankit7987:<Iiitg@123>@cluster0.ggmky.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritmongodb+srv://Ankitr9981:Iiitg123@cluster0.ggmky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     
 }).then(()=>{
     console.log(`connetion successful`);
